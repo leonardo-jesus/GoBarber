e819fcad5e579d8bd7783f69c7ae25aa1c10c918
leonardo-jesus <leonardojesus02@outlook.com>
2021-05-11 19:07:05 -0300

    fix: Change id type to uuid

--- src/database/migrations/1620694575412-CreateAppointments.ts
+++ src/database/migrations/1620694575412-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1620694575412
                 columns: [
                     {
                         name: 'id',
-                        type: 'varchar',
+                        type: 'uuid',
                         isPrimary: true,
                         generationStrategy: 'uuid',
                         default: 'uuid_generate_v4()',
